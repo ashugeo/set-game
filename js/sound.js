1d30f85926d46beee4e3368aa666bf5955977c72
ashugeo <hugo.saintemarie@gmail.com>
2020-11-04 00:37:01 +0100

    Bring volume down

--- js/sound.js
+++ js/sound.js
@@ -3,11 +3,11 @@ export default {
     on: true,
 
     init() {
-        this.sounds['1'] = new Howl({ src: ['/sound/01.wav'] });
-        this.sounds['2'] = new Howl({ src: ['/sound/02.wav'] });
-        this.sounds['3'] = new Howl({ src: ['/sound/03.wav'] });
-        this.sounds['4'] = new Howl({ src: ['/sound/04.wav'] });
-        this.sounds['click'] = new Howl({ src: ['/sound/click.wav'] });
+        this.sounds['1'] = new Howl({ src: ['/sound/01.wav'], volume: .5 });
+        this.sounds['2'] = new Howl({ src: ['/sound/02.wav'], volume: .5 });
+        this.sounds['3'] = new Howl({ src: ['/sound/03.wav'], volume: .5 });
+        this.sounds['4'] = new Howl({ src: ['/sound/04.wav'], volume: .5 });
+        this.sounds['click'] = new Howl({ src: ['/sound/click.wav'], volume: .5 });
     },
 
     play(id) {
