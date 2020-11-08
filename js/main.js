dba6f8279096840c7432a4cf1f403cb77ff4f02b
ashugeo <hugo.saintemarie@gmail.com>
2020-11-08 21:20:13 +0100

    No card transform on touch device

--- js/main.js
+++ js/main.js
@@ -42,12 +42,16 @@ $(document).one('click', '.modes .main', () => {
 });
 
 $(document).on('mouseenter', '.mode > div', e => {
+    if (e.originalEvent.sourceCapabilities.firesTouchEvents) return;
+
     const $el = $(e.currentTarget);
     setTimeout(() => $el.css('transition', 'none'), 100);
     $el.parent('.mode').css('transform', 'translateZ(0) scale(1.03)');
 });
 
 $(document).on('mousemove', '.mode > div', e => {
+    if (e.originalEvent.sourceCapabilities.firesTouchEvents) return;
+
     const $el = $(e.currentTarget);
 
     var ax = (e.pageX - $el.offset().left - $el.outerWidth() / 2) / 20;
@@ -57,6 +61,8 @@ $(document).on('mousemove', '.mode > div', e => {
 });
 
 $(document).on('mouseleave', '.mode > div', e => {
+    if (e.originalEvent.sourceCapabilities.firesTouchEvents) return;
+
     const $el = $(e.currentTarget);
     $el.parent('.mode').css('transform', '');
     $el.css({
