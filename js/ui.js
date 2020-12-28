7b9e1efed66bffa315de1b0b94071c8c3ada5b50
ashugeo <hugo.saintemarie@gmail.com>
2020-12-28 15:39:36 +0100

    Disable card animation for Firefox

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,12 @@
 export default {
     init() {
         const isTouchScreen = window.matchMedia('(pointer: coarse)').matches;
+        const isFirefox = navigator.userAgent.toLowerCase().includes('firefox');
+
+        const disableAnimation = isTouchScreen || isFirefox;
 
         $(document).on('mouseenter', '.mode > div', e => {
-            if (isTouchScreen) return;
+            if (disableAnimation) return;
 
             const $el = $(e.currentTarget);
             setTimeout(() => $el.css('transition', 'none'), 100);
@@ -11,18 +14,18 @@ export default {
         });
         
         $(document).on('mousemove', '.mode > div', e => {
-            if (isTouchScreen) return;
+            if (disableAnimation) return;
 
             const $el = $(e.currentTarget);
         
-            var ax = (e.pageX - $el.offset().left - $el.outerWidth() / 2) / 20;
-            var ay = -(e.pageY - $el.offset().top - $el.outerHeight() / 2) / 20;
+            const ax = (e.pageX - $el.offset().left - $el.outerWidth() / 2) / 15;
+            const ay = -(e.pageY - $el.offset().top - $el.outerHeight() / 2) / 15;
         
             $el.css('transform', `rotateY(${ax}deg) rotateX(${ay}deg)`);
         });
         
         $(document).on('mouseleave', '.mode > div', e => {
-            if (isTouchScreen) return;
+            if (disableAnimation) return;
 
             const $el = $(e.currentTarget);
             $el.parent('.mode').css('transform', '');
